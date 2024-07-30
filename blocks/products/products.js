03fb49f1a69ebd3eedd57593bc1276520a64f2ee
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-07-31 05:23:46 +0530

    Working on the initial layout of the Products block

--- blocks/products/products.js
+++ blocks/products/products.js
@@ -1,12 +1,36 @@
+export class ObjJustVocCompliant {
+  constructor(type, image, href) {
+    this.type = type;
+    this.image = image;
+    this.href = href;
+  }
+}
+
 export function getPathSegments() {
   const pathSegments = window.location.pathname.split('/').filter((segment) => segment);
   // Return the full array of segments
   return pathSegments;
 }
 
-export async function fetchPlaceholders(locale = 'en') {
+const justvocCompliant = (data, vocCompliant, locale) => {
+  const endResult = [];
+  const duplicates = [];
+  let obj = {};
+  data.forEach((entry) => {
+    if (entry['voc-compliant'] === vocCompliant) {
+      if (!duplicates.includes(entry.type)) {
+        duplicates.push(entry.type);
+        obj = new ObjJustVocCompliant(entry.type, entry.image, `/${locale}/products/${entry['voc-compliant']}/${entry.type.toLowerCase().replace(/ /g, '_')}`);
+        endResult.push(obj);
+      }
+    }
+  });
+  return endResult;
+};
+
+export async function fetchPlaceholders(vocCompliant, type, title, locale = 'en') {
   window.placeholders = window.placeholders || {};
-  const TRANSLATION_KEY = 'translation';
+  const TRANSLATION_KEY = 'products';
   const loaded = window.placeholders[`${TRANSLATION_KEY}-loaded`];
 
   if (!loaded) {
@@ -21,19 +45,26 @@ export async function fetchPlaceholders(locale = 'en') {
         .then((json) => {
           const placeholders = {};
           const KEY = 'Key';
-          console.log(json.data);
+
+          if (typeof type === 'undefined' && typeof title === 'undefined') {
+            const endResult = justvocCompliant(json.data, vocCompliant, locale);
+            console.log(endResult);
+          }
 
           json.data.forEach((entry) => {
             Object.keys(entry).forEach((localeKey) => {
-              if (localeKey !== KEY) {
-                if (placeholders[localeKey]) {
-                  placeholders[localeKey][entry.Key.toLowerCase()] = entry[localeKey];
-                } else {
-                  placeholders[localeKey] = {
-                    [entry.Key.toLowerCase()]: entry[localeKey],
-                  };
-                }
-              }
+
+
+
+            //   if (localeKey !== KEY) {
+            //     if (placeholders[localeKey]) {
+            //       placeholders[localeKey][entry.Key.toLowerCase()] = entry[localeKey];
+            //     } else {
+            //       placeholders[localeKey] = {
+            //         [entry.Key.toLowerCase()]: entry[localeKey],
+            //       };
+            //     }
+            //   }
             });
           });
 
@@ -53,6 +84,5 @@ export async function fetchPlaceholders(locale = 'en') {
 export default async function decorate(block) {
   const [locale, products, vocCompliant, type, title] = getPathSegments();
   console.log(locale, products, vocCompliant, type, title);
-  const placeholders = await fetchPlaceholders(locale);
-  console.log(placeholders);
+  const placeholders = await fetchPlaceholders(vocCompliant, type, title, locale);
 }
