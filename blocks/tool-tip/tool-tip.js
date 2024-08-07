d4c6c4954ce80d03d653ab5b7d49cacd03cf834c
dave-fink <david@keypixel.com>
2024-08-07 10:30:34 -0400

    adding - tool-tip

--- blocks/tool-tip/tool-tip.js
+++ blocks/tool-tip/tool-tip.js
@@ -1,16 +1,15 @@
-import { div, a } from '../../scripts/dom-helpers.js';
+/* eslint-disable function-paren-newline, object-curly-newline */
+import { div, br } from '../../scripts/dom-helpers.js';
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
 function createTip(row, i) {
   const tipFrag = document.createDocumentFragment();
-
-  // Create the tip-info element
-  const $tip = div({ class: 'agencieinfo', id: `box${i}` });
+  const $tip = div({ class: 'info', id: `box${i}` });
   tipFrag.appendChild($tip);
 
   row.querySelectorAll(':scope > div').forEach((col, index) => {
     if (index === 0) { // first column = textual content
-      $tip.appendChild(col);
+      $tip.innerHTML = col.innerHTML;
     } else if (index === 1) { // second column = marker data
       // Regex to match the location, x and y coordinates
       const regex = /([^,[\]]+)\s*\[(\d+px),\s*(\d+px)]/g;
@@ -20,7 +19,9 @@ function createTip(row, i) {
         const location = match[1].trim();
         const x = match[2];
         const y = match[3];
-        const $marker = a({ class: 'pointer', title: location, style: ` top: ${x}; left: ${y};` });
+        const $marker = div({
+          class: 'pin', 'data-id': `box${i}`, title: location, style: ` top: ${x}; left: ${y};`,
+        });
         tipFrag.appendChild($marker);
       }
     }
@@ -28,60 +29,46 @@ function createTip(row, i) {
   return tipFrag;
 }
 
-
-function mapStuff(block, $welcomebox, $canvas) {
-
-  // document.addEventListener('DOMContentLoaded', () => {
-
-  const _boxes = {
-    box1: ['us', 'southus'],
-    box2: ['eu', 'af', 'ru'],
-    box3: ['dubai'],
-    box5: ['aus', 'asia'],
-  };
-
-  const _pointers = {
-    us: [80, 80],
-    southus: [150, 220],
-    eu: [268, 58],
-    dubai: [340, 120],
-    aus: [506, 237],
-    af: [300, 150],
-    asia: [400, 126],
-    ru: [300, 30],
-  };
-
-
-  // Function to fade in an element
-  function fadeIn(element, opacity) {
-    let op = 0; // initial opacity
-    const timer = setInterval(() => {
-      if (op >= opacity) {
-        clearInterval(timer);
-        element.style.opacity = opacity;
-      }
-      element.style.opacity = op;
-      op += 0.05;
-    }, 50);
+function fade(dir, el) {
+  if (dir === 'in') {
+    el.style.visibility = 'visible';
+    el.classList.add('fade-in');
+  } else {
+    el.classList.remove('fade-in');
+    setTimeout(() => { el.style.visibility = 'hidden'; }, 1600);
   }
+}
 
-  // Function to fade out an element
-  function fadeOut(element, opacity) {
-    let op = 1; // initial opacity
-    const timer = setInterval(() => {
-      if (op <= opacity) {
-        clearInterval(timer);
-        element.style.opacity = opacity;
-      }
-      element.style.opacity = op;
-      op -= 0.05;
-    }, 50);
-  }
+export default function decorate(block) {
+  const rows = block.querySelectorAll(':scope > div');
+  const $image = createOptimizedPicture(rows[0].querySelector('img').src, 'title', true, [{ width: '600' }]);
+  const $tips = div({ class: 'tips' });
 
-  if ($welcomebox) {
-    fadeIn($welcomebox, 0.6);
+  Array.from(rows)
+    .splice(1)
+    .forEach((row, i) => {
+      const $tip = createTip(row, i);
+      $tips.append($tip);
+    });
+
+  const $canvas = document.createElement('canvas');
+  $canvas.width = 580;
+  $canvas.height = 538;
+
+  const $intro = div({ class: 'intro' },
+    'Click a pointer on the map',
+    br(),
+    'to show information about the agency.',
+  );
+
+  block.replaceWith($image, $tips, $canvas, $intro);
+
+  if ($intro) {
     setTimeout(() => {
-      fadeOut($welcomebox, 0);
+      fade('in', $intro);
+    }, 100);
+    setTimeout(() => {
+      fade('out', $intro);
     }, 4000);
   }
 
@@ -89,147 +76,75 @@ function mapStuff(block, $welcomebox, $canvas) {
   ctx.save();
   ctx.clearRect(0, 0, $canvas.width, $canvas.height);
 
-  const agencies = document.querySelector('.tips');
-  if (agencies) {
-    const pointers = agencies.querySelectorAll('.pointer');
+  // create clickable canvas
+  if ($tips) {
+    const $pins = $tips.querySelectorAll('.pin');
 
-    console.log(pointers);
-    function clearCanvas() {
+    const clearCanvas = () => {
       ctx.clearRect(0, 0, $canvas.width, $canvas.height);
-    }
+    };
 
-    function clearAll() {
-      if ($welcomebox) fadeOut($welcomebox, 0);
+    const clearAll = () => {
+      if ($intro.style.visibility === 'visible')fade('out', $intro);
       clearCanvas();
-      const agencieInfos = agencies.querySelectorAll('.agencieinfo');
-      agencieInfos.forEach((item) => {
-        item.style.visibility = 'hidden';
+      const activeItems = $tips.querySelectorAll('.active');
+      activeItems.forEach((item) => {
+        item.classList.remove('active');
       });
-      pointers.forEach((pointer) => {
-        pointer.classList.remove('active');
-      });
-    }
+    };
 
-    function drawCanvas(box, clearCanvas) {
-      console.log('box', box);
-      if (clearCanvas) {
-        clearCanvas();
-      } else {
-        clearAll();
-      }
+    const drawCanvas = (boxID, clear) => {
+      const aBox = document.getElementById(boxID);
+      const aPins = document.querySelectorAll(`[data-id="${boxID}"]`);
 
-      box.style.visibility = 'visible';
+      if (clear) clearCanvas();
+      else clearAll();
 
-      const boxRect = {
-        left: parseInt(window.getComputedStyle(box).left, 10),
-        top: parseInt(window.getComputedStyle(box).top, 10),
-        height: parseInt(window.getComputedStyle(box).height, 10),
-        width: parseInt(window.getComputedStyle(box).width, 10),
+      // set box active and get position
+      aBox.classList.add('active');
+      const aBoxRect = {
+        left: parseInt(window.getComputedStyle(aBox).left, 10),
+        top: parseInt(window.getComputedStyle(aBox).top, 10),
+        height: parseInt(window.getComputedStyle(aBox).height, 10),
+        width: parseInt(window.getComputedStyle(aBox).width, 10),
         center: {
-          // eslint-disable-next-line max-len
-          left: parseInt(window.getComputedStyle(box).left, 10) + (parseInt(window.getComputedStyle(box).width, 10) / 2),
-          // eslint-disable-next-line max-len
-          top: parseInt(window.getComputedStyle(box).top, 10) + (parseInt(window.getComputedStyle(box).height, 10) / 2),
+          left: parseInt(window.getComputedStyle(aBox).left, 10)
+            + (parseInt(window.getComputedStyle(aBox).width, 10) / 2),
+          top: parseInt(window.getComputedStyle(aBox).top, 10)
+            + (parseInt(window.getComputedStyle(aBox).height, 10) / 2),
         },
       };
 
-
-
-      _boxes[box.id].forEach((item) => {
+      // set pin active and draw lines
+      aPins.forEach((pin) => {
+        pin.classList.add('active');
+        const x = parseInt(pin.style.left, 10);
+        const y = parseInt(pin.style.top, 10);
         ctx.strokeStyle = 'rgba(254,138,2,1)';
         ctx.lineWidth = 2;
         ctx.beginPath();
-        ctx.moveTo(_pointers[item][0] + 5, _pointers[item][1] + 5);
-        ctx.lineTo(boxRect.center.left, boxRect.center.top);
+        ctx.moveTo(x + 5, y + 5);
+        ctx.lineTo(aBoxRect.center.left, aBoxRect.center.top);
         ctx.stroke();
         ctx.closePath();
-
-        document.getElementById(item)
-          .classList
-          .add('active');
       });
-    }
+    };
 
-    const agencieInfos = agencies.querySelectorAll('.agencieinfo');
+    // add listeners
+    $canvas.addEventListener('click', clearAll);
 
+    const $infoBoxes = $tips.querySelectorAll('.info');
 
-    agencieInfos.forEach((item) => {
-      console.log('items', item.innerHTML);
-      item.dataset.initPos = JSON.stringify({
-        left: window.getComputedStyle(item).left,
-        top: window.getComputedStyle(item).top,
-      });
+    // prevent info boxes from closing when clicked
+    $infoBoxes.forEach((item) => item.addEventListener('click', (e) => e.stopPropagation()));
 
-      // Assuming you want drag functionality, you may need to use a library or implement it
-      // For native drag-and-drop, you'll have to use the HTML5 Drag and Drop API or similar
-      item.addEventListener('drag', () => {
-        drawCanvas(item, true);
-      });
-    });
-
-    agencies.addEventListener('click', clearAll);
-    agencieInfos.forEach((item) => {
-      item.addEventListener('click', (e) => {
-        e.stopPropagation();
-      });
-    });
-
-    pointers.forEach((pointer) => {
-      pointer.addEventListener('click', (e) => {
-        let boxName;
-
-        Object.keys(_boxes)
-          .forEach((key) => {
-            if (_boxes[key].includes(pointer.id)) {
-              boxName = key;
-            }
-          });
-
-        console.log('boxName', boxName);
-        const box = document.getElementById(boxName);
-        drawCanvas(box);
+    $pins.forEach((pin) => {
+      pin.addEventListener('click', (e) => {
+        drawCanvas(pin.getAttribute('data-id'));
         e.stopPropagation();
       });
     });
   }
 
   ctx.restore();
-  // });
-
-}
-
-
-export default function decorate(block) {
-  const rows = block.querySelectorAll(':scope > div');
-
-  // block add id agencies
-  block.id = 'agencies';
-
-  const $image = createOptimizedPicture(rows[0].querySelector('img').src, 'title', true, [{ width: '600' }]);
-
-  const $tipContainer = div({ class: 'tips' });
-
-  Array.from(rows)
-    .splice(1)
-    .forEach((row, i) => {
-      const $tip = createTip(row, i);
-      $tipContainer.append($tip);
-    });
-
-  const $canvas = document.createElement('canvas');
-  $canvas.id = 'canvas';
-  $canvas.width = 580;
-  $canvas.height = 538;
-
-  const $welcomebox = div({ id: 'welcomebox' },
-    'Click a pointer on the map to show information about the agency.',
-  );
-
-  block.replaceWith($image, $tipContainer, $canvas, $welcomebox);
-
-  // set timeout for 1 second
-  setTimeout(() => {
-    mapStuff(block, $welcomebox, $canvas);
-  }, 1000);
-
 }
