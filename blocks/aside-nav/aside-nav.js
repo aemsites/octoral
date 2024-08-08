95741538640730cd418af922f376de189384a2c1
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-08-09 02:49:26 +0530

    Fixing the lint issues

--- blocks/aside-nav/aside-nav.js
+++ blocks/aside-nav/aside-nav.js
@@ -75,11 +75,11 @@ function replaceEntries(placeholders, element) {
 }
 
 export default async function decorate(block) {
-  var [locale] = getPathSegments();
+  let [locale] = getPathSegments();
   const regions = ['en', 'de', 'es', 'fr', 'it', 'nl', 'us'];
   if (!regions.some((region) => region === locale)) {
     locale = 'en';
-   }
+  }
   currentPath = window.location.pathname;
   const placeholders = await fetchPlaceholders(locale);
   [...block.children].forEach((row) => {
