f592d6fb5310e9a5c0cc89a198eac59d947be219
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-09-04 23:43:04 +0530

    Injecting Cookie consent code

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -1,1 +1,8 @@
 // add delayed functionality here
+import { loadScript } from './aem.js';
+
+async function loadConsentManager() {
+  await loadScript('https://nexus.ensighten.com/sherwin/p-octoral_gdpr/Bootstrap.js');
+}
+
+await loadConsentManager();
