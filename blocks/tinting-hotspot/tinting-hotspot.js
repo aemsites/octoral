6b25beccdd38d75e1914f88fdf6140c166b1c8e2
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-09-11 05:19:19 +0530

    Enabling hotspot further

--- blocks/tinting-hotspot/tinting-hotspot.js
+++ blocks/tinting-hotspot/tinting-hotspot.js
@@ -1,5 +1,19 @@
+import { div } from '../../scripts/dom-helpers.js';
+
 export default function decorate(block) {
-  [...block.children].forEach((row) => {
-    console.log(row);
+  const leftDiv = document.querySelector('.left.section');
+
+  [...block.children].forEach((row, r) => {
+    if (r > 0) {
+      const nexticondiv = div({ class: 'hotspot' });
+      nexticondiv.classList.add([...row.children][0].textContent);
+      const data = [...row.children][0].innerHTML;
+      nexticondiv.addEventListener('click', () => {
+        leftDiv.innerHTML = data;
+      });
+      console.log(nexticondiv);
+      row.after(nexticondiv);
+      row.remove();
+    }
   });
 }
