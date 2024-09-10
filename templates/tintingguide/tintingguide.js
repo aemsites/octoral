e792b640b807fcb533746f78e560f2e4a7327aea
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-09-11 02:37:45 +0530

    Adding a template for tintingguide

---
+++ templates/tintingguide/tintingguide.js
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars,no-empty-function
+import { loadTemplate } from '../../scripts/scripts.js';
+
+export default async function decorate(doc) {
+  doc.querySelector('header').remove();
+  doc.querySelector('footer').remove();
+  doc.body.classList.add('tintingguide');
+}
