7fc7402803bd10694a7e96643a1096905f54f234
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-09-12 03:37:24 +0530

    Updating the CSS further

--- blocks/tinting-hotspot/tinting-hotspot.js
+++ blocks/tinting-hotspot/tinting-hotspot.js
@@ -1,8 +1,10 @@
-import { div } from '../../scripts/dom-helpers.js';
+import { div, button } from '../../scripts/dom-helpers.js';
 
 export default function decorate(block) {
   const leftDiv = document.querySelector('.left.section');
   const rightDiv = document.querySelector('.right.section');
+  const navDiv = div({ class: 'nav' }, button('X'));
+  const contentDiv = div({ class: 'content' });
 
   [...block.children].forEach((row, r) => {
     if (r > 0) {
@@ -10,7 +12,10 @@ export default function decorate(block) {
       nexticondiv.style.margin = `${[...row.children][2].textContent} 0 0 ${[...row.children][3].textContent}`;
       const data = [...row.children][1].innerHTML;
       nexticondiv.addEventListener('click', () => {
-        leftDiv.innerHTML = data;
+        leftDiv.innerHTML = '';
+        contentDiv.innerHTML = data;
+        leftDiv.appendChild(navDiv);
+        leftDiv.appendChild(contentDiv);
         leftDiv.classList.remove('hidden');
         rightDiv.classList.add('postclick');
       });
