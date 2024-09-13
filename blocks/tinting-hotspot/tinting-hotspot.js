8d4c37843e8722a10f7ead93e34c9462983bc261
meejain <37831850+meejain@users.noreply.github.com>
2024-09-13 18:51:27 +0530

    Issue109 Tingting Guide Improvization (#112)
    * Adding co ordinates for the margins on the document
    
    * Updating the CSS further
    
    * Updating the CSS further
    
    * Updating the CSS further
    
    * Updatint the logic for button clicking
    
    * Taking care of Mobile view
    
    * Taking care of Mobile view
    
    * Improvizing on the mobile view further
    
    * Nesting the CSS
    
    * Updating the logic for closing rest of the popups once we click on any hotspot
    
    * Updating the logic for popup opening at the same coordinates as the hotspot
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>

--- blocks/tinting-hotspot/tinting-hotspot.js
+++ blocks/tinting-hotspot/tinting-hotspot.js
@@ -1,20 +1,52 @@
-import { div } from '../../scripts/dom-helpers.js';
+import { div, button } from '../../scripts/dom-helpers.js';
+
+// media query match that indicates mobile/tablet width
+const isDesktop = window.matchMedia('(min-width: 900px)');
 
 export default function decorate(block) {
   const leftDiv = document.querySelector('.left.section');
   const rightDiv = document.querySelector('.right.section');
+  const navDiv = div({ class: 'nav' }, button('X'));
+  navDiv.querySelector('button').addEventListener('click', () => {
+    if (isDesktop.matches) {
+      leftDiv.classList.add('hidden');
+      rightDiv.classList.remove('postclick');
+    } else {
+      navDiv.parentElement.remove();
+    }
+  });
+  const contentDiv = div({ class: 'content' });
 
   [...block.children].forEach((row, r) => {
     if (r > 0) {
       const nexticondiv = div({ class: 'hotspot' });
-      nexticondiv.classList.add([...row.children][0].textContent);
+      nexticondiv.style.margin = `${[...row.children][2].textContent} 0 0 ${[...row.children][3].textContent}`;
       const data = [...row.children][1].innerHTML;
       nexticondiv.addEventListener('click', () => {
-        leftDiv.innerHTML = data;
+        leftDiv.innerHTML = '';
+        contentDiv.innerHTML = data;
+        leftDiv.appendChild(navDiv);
+        leftDiv.appendChild(contentDiv);
         leftDiv.classList.remove('hidden');
-        rightDiv.classList.add('postclick');
+        if (isDesktop.matches) {
+          leftDiv.innerHTML = '';
+          contentDiv.innerHTML = data;
+          leftDiv.appendChild(navDiv);
+          leftDiv.appendChild(contentDiv);
+          rightDiv.classList.add('postclick');
+        } else {
+          leftDiv.innerHTML = '';
+          block.parentElement.parentElement.querySelectorAll('.popup').forEach((popup) => {
+            popup.remove();
+          });
+          const popupDiv = div({ class: 'popup' });
+          popupDiv.style.margin = `${(Number([...row.children][2].textContent.replace('%', '')) + 60).toString()}% 0 0 0`;
+          contentDiv.innerHTML = data;
+          popupDiv.appendChild(navDiv);
+          popupDiv.appendChild(contentDiv);
+          nexticondiv.after(popupDiv);
+        }
       });
-      console.log(nexticondiv);
       row.after(nexticondiv);
       row.remove();
     }
