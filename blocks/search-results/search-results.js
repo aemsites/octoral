22f46670d0c5a4b94b311f2feb1926d6c11c3492
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-09-19 04:41:08 +0530

    Adding the initial logic for search functionality

---
+++ blocks/search-results/search-results.js
@@ -0,0 +1,28 @@
+import ffetch from '../../scripts/ffetch.js';
+import { getPathSegments } from '../../scripts/utils.js';
+
+function searchItems(searchTerm) {
+  const tokenizedSearchWords = searchTerm.split(' ');
+  if (tokenizedSearchWords.length > 1) tokenizedSearchWords.unshift(searchTerm);
+  return tokenizedSearchWords;
+}
+
+async function loadResultsFromNews(tokenizedSearchWords, resultsDiv) {
+  const [rawLocale, , , ,] = getPathSegments();
+  const jsonData = await ffetch(`/${rawLocale}/news/query-index.json`)
+    .chunks(1000)
+    .all();
+  console.log(jsonData);
+  console.log(tokenizedSearchWords);
+  console.log(rawLocale);
+  console.log(window.location.hostname);
+}
+
+export default async function decorate(block) {
+  block.innerHTML = '';
+  const searchTerm = new URLSearchParams(window.location.search).get('query');
+  if (searchTerm) {
+    const tokenizedSearchWords = searchItems(searchTerm);
+    loadResultsFromNews(tokenizedSearchWords, block);
+  }
+}
