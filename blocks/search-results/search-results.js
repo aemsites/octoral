430077ba703fc37069cdf2a2e9b0ad78b9ffa653
Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>
2024-09-19 22:36:41 +0530

    Adding the logic of search for others

--- blocks/search-results/search-results.js
+++ blocks/search-results/search-results.js
@@ -1,6 +1,16 @@
 import ffetch from '../../scripts/ffetch.js';
 import { getPathSegments } from '../../scripts/utils.js';
 
+class SearchObj {
+  // eslint-disable-next-line max-len
+  constructor(searchTitle, searchDescription, searchPath, searchPublished) {
+    this.searchTitle = searchTitle;
+    this.searchDescription = searchDescription;
+    this.searchPath = searchPath;
+    this.searchPublished = searchPublished;
+  }
+}
+
 function searchItems(searchTerm) {
   const tokenizedSearchWords = searchTerm.split(' ');
   if (tokenizedSearchWords.length > 1) tokenizedSearchWords.unshift(searchTerm);
@@ -12,8 +22,35 @@ function filterMatches(tokenizedSearchWords, jsonData) {
   tokenizedSearchWords.forEach((searchTerm) => {
     const matches = jsonData.filter((entry) => (
       entry.path
-      + entry.title
-      + entry.description
+        + entry.title
+        + entry.description
+    )
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase()));
+    allMatches.push(...matches);
+  });
+  // remove duplicates:
+  return [...new Set(allMatches)];
+}
+
+function filterProductMatches(tokenizedSearchWords, jsonData) {
+  const allMatches = [];
+  tokenizedSearchWords.forEach((searchTerm) => {
+    const matches = jsonData.filter((entry) => (
+      entry['voc-compliant']
+        + entry['voc-compliant-label']
+        + entry['voc-compliant-desc']
+        + entry.type
+        + entry['type-label']
+        + entry['type-desc']
+        + entry.title
+        + entry['title-label']
+        + entry['title-desc']
+        + entry['sub-title']
+        + entry['item-nr']
+        + entry.code
+        + entry['product-name']
+        + entry.volume
     )
       .toLowerCase()
       .includes(searchTerm.toLowerCase()));
@@ -24,6 +61,7 @@ function filterMatches(tokenizedSearchWords, jsonData) {
 }
 
 async function loadResultsFromNews(tokenizedSearchWords, resultsDiv) {
+  const searchResults = [];
   console.log(resultsDiv);
   const [rawLocale, , , ,] = getPathSegments();
   window.placeholders = window.placeholders || {};
@@ -38,11 +76,18 @@ async function loadResultsFromNews(tokenizedSearchWords, resultsDiv) {
   const jsonDataOthers = await ffetch('/query-index.json')
     .chunks(1000)
     .all();
+  const json = window.placeholders.products[`${rawLocale}`];
+  const jsonDataProducts = json.data;
 
   const matchesNews = filterMatches(tokenizedSearchWords, jsonDataNews);
-  console.log(matchesNews);
   const matchesOthers = filterMatches(tokenizedSearchWords, jsonDataOthers);
-  console.log(matchesOthers);
+  [...matchesNews, ...matchesOthers].forEach((entry) => {
+    const obj = new SearchObj(entry.title, entry.description, entry.path, entry.publishDate);
+    searchResults.push(obj);
+  });
+  console.log(searchResults);
+  const matchesProducts = filterProductMatches(tokenizedSearchWords, jsonDataProducts);
+  console.log(matchesProducts);
 }
 
 export default async function decorate(block) {
