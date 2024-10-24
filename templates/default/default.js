d156df98e7789ebdf796495cfd68192770359d90
meejain <37831850+meejain@users.noreply.github.com>
2024-10-24 13:13:25 +0530

    Mobile view enhancement (#133)
    * Working on the Mobile view
    
    * Working on the Mobile view
    
    * Working on the Mobile view
    
    * Working on the Mobile view
    
    * Fixing the lint issues
    
    * Adding an event listner for window resize
    
    * Adding an event listner for window resize
    
    * Fixing the lint issues
    
    * Fixing the CSS nesting
    
    ---------
    
    Co-authored-by: Meet Kumar Jain <meejain@Meets-MacBook-Pro-2.local>

--- templates/default/default.js
+++ templates/default/default.js
@@ -1,6 +1,32 @@
 // eslint-disable-next-line no-unused-vars,no-empty-function
-import { aside, section } from '../../scripts/dom-helpers.js';
+import {
+  aside, section, span, div,
+} from '../../scripts/dom-helpers.js';
 import { loadFragment } from '../../blocks/fragment/fragment.js';
+
+const isDesktop = window.matchMedia('(min-width: 800px)');
+let $aside = '';
+let $hamburger = '';
+
+function mobileAction() {
+  $aside.classList.add('off-screen-menu');
+}
+
+function resizeAction() {
+  if (isDesktop.matches) {
+    if ($aside.classList.contains('off-screen-menu')) {
+      $aside.classList.remove('off-screen-menu');
+    }
+    if ($hamburger.classList.contains('active')) {
+      $hamburger.classList.remove('active');
+    }
+    if ($aside.classList.contains('active')) {
+      $aside.classList.remove('active');
+    }
+  } else {
+    mobileAction();
+  }
+}
 
 export default async function decorate(doc) {
   const $main = doc.querySelector('main');
@@ -13,8 +39,22 @@ export default async function decorate(doc) {
 
   const asideNavFrag = await loadFragment('/aside-nav');
   const $asideNav = asideNavFrag.querySelector('.aside-nav-wrapper').cloneNode(true);
-  const $aside = aside($asideNav);
+  $aside = aside($asideNav);
+  const $divleft = div({ class: 'mobilemenu' });
+
+  // Creation of Hamburger Menu
+  $hamburger = div({ class: 'ham-menu' }, span(), span(), span());
+  $hamburger.addEventListener('click', () => {
+    $hamburger.classList.toggle('active');
+    $aside.classList.toggle('active');
+  });
 
   $main.innerHTML = '';
-  $main.append($aside, $section);
+  $divleft.append($hamburger, $aside);
+  $main.append($divleft, $section);
+
+  if (!isDesktop.matches) {
+    mobileAction();
+  }
+  window.addEventListener('resize', resizeAction);
 }
